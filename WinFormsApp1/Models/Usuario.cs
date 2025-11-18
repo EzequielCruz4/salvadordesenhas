356b4e42f64f9650ad83605f6417193a6eb7e502
Ezequiel Cruz <ezequielfcruzsousa@gmail.com>
2025-11-18 20:10:21 -0300

    Delete WinFormsApp1 directory

--- WinFormsApp1/Models/Usuario.cs
+++
@@ -1,9 +0,0 @@
-namespace WinFormsApp1.Models
-{
-    internal class Usuario
-    {
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public string Endereco { get; set; }
-    }
-}
